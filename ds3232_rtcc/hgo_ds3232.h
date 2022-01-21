006868f123aa3754c8f1a0662ec5392d50813729
HgO01 <94792588+HgO01@users.noreply.github.com>
2022-01-21 18:31:37 +0100

    Update and rename ds3232.c to hgo_ds3232.h

--- ds3232_rtcc/hgo_ds3232.h
+++ ds3232_rtcc/hgo_ds3232.h
@@ -1,27 +1,24 @@
 
+#include <stdbool.h>
 #include <xc.h>
-#include "i2c.h"
 
+#include "hgo_i2c.h"
 
 //
 void rtcc_write(unsigned char slave_addr,unsigned char word_adrr, unsigned char data);
-unsigned char rtcc_read(unsigned addr);
+unsigned char rtcc_read(unsigned char slave_addr, unsigned char word_addr);
 
 //
 void set_time(int seconds, int minutes, int hours);
 void set_alarm1_time(int seconds, int minutes, int hours);
-int get_seconds(unsigned char , unsigned char);
-int get_minutes(unsigned char, unsigned char);
-int get_hours(unsigned char, unsigned char);
+int get_seconds();
+int get_minutes();
+int get_hours();
+bool is_alarm1_ready();
+bool is_alarm2_ready();
+
 void rtcc_config();
 
-void main(void)
-{
-    
-    
-    
-    return;
-}
 
 void rtcc_write(unsigned char slave_addr,unsigned char word_addr, unsigned char data)
 {
@@ -123,17 +120,62 @@ void set_alarm2_time(int minutes, int hours)
 
 
 
-int get_seconds(unsigned char slave_addr, unsigned char word_addr)
+int get_seconds()
 {
-    int seconds = bcd_to_int(rtcc_read(slave_addr, word_addr));
+    int seconds = bcd_to_int(rtcc_read(0xD0, 0x00));
     
     return seconds;
 }
 
-int get_minute(unsigned char addr, unsigned char word_addr)
+
+int get_minute()
 {
-    int minute = bcd_to_int(r)
+    int minute = bcd_to_int(rtcc_read(0xD0, 0x01));
+    
+    return minute;
 }
+
+int get_hour()
+{
+    int hour = bcd_to_int(rtcc_read(0xD0, 0x02));
+    
+    return hour;
+}
+
+bool is_alarm1_ready()
+{
+    unsigned char alarm_IF;
+    
+    alarm_IF = rtcc_read(0xD0, 0x0F);
+    
+    
+    if(alarm_IF&0x01) 
+    {
+        rtcc_write(0xD0, 0x0F, 0x00);
+        return true;
+    }
+    
+    return false;
+    
+}
+
+bool is_alarm2_ready()
+{
+    unsigned char alarm_IF;
+    
+    alarm_IF = rtcc_read(0xD0, 0x0F);
+    
+    
+    if(alarm_IF&0x02) 
+    {
+        rtcc_write(0xD0, 0x0F, 0x00);
+        return true;
+    }
+    
+    return false;
+    
+}
+
 unsigned char int_to_bcd(int int_val)
 {
     return((int_val/10)<<4 |int_val%10);
