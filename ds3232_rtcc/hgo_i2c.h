6737805183779723b836f099f5982d1be1bfecca
HgO01 <94792588+HgO01@users.noreply.github.com>
2022-03-09 12:16:53 +0100

    Update hgo_i2c.h

--- ds3232_rtcc/hgo_i2c.h
+++ ds3232_rtcc/hgo_i2c.h
@@ -1,101 +1,60 @@
 
-void i2c_init();
-void i2c_start();
-void i2c_stop();
-void i2c_wait();
-void i2c_write();
-void i2c_read();
-void i2c_ack();
-void i2c_nack();
-void i2c_repeated_start();
+/********************** I2C functions **************************/
 
-void i2c_init()
-{
-    
-    
-    //////////////
-    // Fosc = 8MHz
-    //
-    //////////////
-    SSPADD = 0x28; //set baud rate to 100khZ
-    
-    SSPEN = 1; //SSP enabled
-    
-    //SSPM = 8; 
-    SSPM3 = 1;
-    SSPM2 = 0;
-    SSPM1 = 0;
-    SSPM0 = 0;
-    
-    
-    ////////////Pin Configurations///////////
-    //SDA = 
-    //SCL = OUTPUT
-    ////////////////////////////////////////
-    
-    TRISCbits.TRISC3 = 1; // SCL
-    TRISCbits.TRISC4 = 1; // SDA
-    
-    
-    /////////Interrupt Settings //////////////////
-    //
-    //
-    //
-    /////////////////////////////////////////////
-    GIE = 1; //enable global interrupt or  //asm("ie");
-    PEIE = 1; //enable peripheral interrupt
-    SSPIF = 0; //clear SSPIF
-    SSPIE = 1; // enable SSP interrupt
-   
-    
-}
-
+//don't forget to enter i2c frequency
+// tested with 100kHz
 
 void i2c_wait()
 {
-    while(SSPIF);
-    SSPIF = 0;
+    while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F));  // wait for MSSP module to be free (not busy)
 }
-
-
-void start()
+ 
+void I2C_Init(uint32_t i2c_clk_freq)
+{
+    SSPCON  = 0x28;  // configure MSSP module to work in I2C mode
+    SSPADD  = (_XTAL_FREQ/(4 * i2c_clk_freq)) - 1;  // set I2C clock frequency
+    SSPSTAT = 0;
+}
+ 
+void I2C_Start()
 {
     i2c_wait();
-    SEN = 1; //start enable
+    SEN = 1;  // initiate start condition
 }
-
-void stop()
+ 
+void I2C_Repeated_Start()
 {
     i2c_wait();
-    PEN = 1; //stop enabled
+    RSEN = 1;  // initiate repeated start condition
 }
-
-void ack()
+ 
+void I2C_Stop()
 {
     i2c_wait();
-    ACKEN = 1; //enable ack or nack
-    ACKDT = 0; //ACK enable
+    PEN = 1;  // initiate stop condition
 }
-
-void nack()
+ 
+void I2C_Write(uint8_t i2c_data)
 {
     i2c_wait();
+    SSPBUF = i2c_data;  // update buffer
+}
+ 
+uint8_t I2C_Read(uint8_t ack)
+{
+    uint8_t _data;
+    i2c_wait();
+    RCEN = 1;
+    i2c_wait();
+    _data = SSPBUF;  // read data from buffer
+    i2c_wait();
+    // send acknowledge pulse ? (depends on ack, if 1 send, otherwise don't send)
+    ACKDT = !ack;
     ACKEN = 1;
-    ACKDT = 1;
+    return _data;  // return data read
 }
-
-void i2c_write(unsigned char data)
-{
-    i2c_wait();
-    SSPBUF = data;
-}
-
-unsigned char i2c_read()
-{
-    i2c_wait();
-    return SSPBUF;
-}
-
+ 
+/********************** end I2C functions **********************/
 void i2c_repeated_start()
 {
     i2c_wait();
